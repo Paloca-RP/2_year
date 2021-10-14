c58fbf7ab5172d9eba30d76cb33a5fd6b562016f
Paloca-RP <ricardopalhoca@hotmail.com>
2021-10-14 22:14:30 +0100

    14/10

---
+++ Programacao_visual/ProgVis2021/Semana01/A_Slide/A_Slide/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TP01_Slides
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int numint = 5, numint2 = 6, aux;
+            aux = numint;
+            numint = numint2;
+            numint2 = aux;
+
+            bool b = (numint == 5);
+            Console.WriteLine(b);
+        }
+    }
+}
