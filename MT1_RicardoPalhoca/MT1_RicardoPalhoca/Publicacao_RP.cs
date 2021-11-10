d7aab687c6f5abebba9a14d32420fca631844e05
Paloca-RP <ricardopalhoca@hotmail.com>
2021-11-10 18:37:02 +0000

    m

---
+++ MT1_RicardoPalhoca/MT1_RicardoPalhoca/Publicacao_RP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT1_RicardoPalhoca
+{
+    class Publicacao_RP
+    {
+        private string titulo_RP;
+
+        public string Titulo_RP
+        {
+            get { return titulo_RP; }
+            set { titulo_RP = value; }
+        }
+
+        public Publicacao_RP(String titulo_RP)
+        {
+            this.Titulo_RP = titulo_RP;
+        }
+
+        public Publicacao_RP() : this("Sem Titulo")
+        {
+        }
+
+        override public String ToString()
+        {
+            return Titulo_RP + "\n";
+        }
+
+    }
+}
