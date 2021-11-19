c2c20d47a9c3bcdb2ab574413f1e95e646e7cb3d
Ricardo Palhoça <73611099+Paloca-RP@users.noreply.github.com>
2021-11-19 15:50:47 +0000

    lab 3 PIS

---
+++ Programacao_Integracao_Servicos/Lab3/Lab3/app.js
@@ -0,0 +1,73 @@
+//Nivel 1
+/*const express = require("express")
+const app = express()
+
+app.use('/rotas', function(req, res, next){ 
+    console.log("A request for 'rotas' received at " + Date.now()); 
+    next(); 
+ }); 
+  
+ app.get('/rotas', function(req, res){ 
+    res.send('teste'); 
+ });
+
+app.listen(8081, function() {
+    console.log("http://localhost:8081")
+})
+*/
+//Nivel 2
+/*var express = require('express'); 
+var app = express(); 
+ 
+//Middleware: sempre executado 
+app.use(function(req, res, next){ 
+   console.log("Start"); 
+   next(); //importante para continuar o processamento 
+}); 
+ 
+//Rota: apenas executada porque o app.use anterior tem o next() 
+app.get('/', function(req, res, next){
+    res.send("Middle"); 
+   next(); //importante para continuar o processamento 
+}); 
+ 
+//Middleware: apenas executada porque o app.get anterior tem o next() 
+app.use('/', function(req, res){ 
+   console.log('End'); //finaliza por isso não precisa do next() 
+}); 
+ 
+app.listen(8081);
+*/
+//Nivel 3
+/*var express = require('express'); 
+var app = express(); 
+ 
+var urls = []; 
+ 
+//Middleware: sempre executado 
+app.use(function (req, res, next) { 
+    if (urls[req.url]) { 
+        urls[req.url] += 1; 
+    } else { 
+        urls[req.url] = 1; 
+    } 
+ 
+    console.log(urls.sort()); 
+    next(); 
+}); 
+ 
+app.get('/', function(req, res, next){ 
+   res.send("Root"); 
+}); 
+ 
+app.get('/rota/subrota1', function(req, res, next){ 
+   res.send("Rota 1"); 
+}); 
+ 
+app.get('/rota/subrota2', function(req, res, next){ 
+   res.send("Rota 2"); 
+}); 
+
+app.listen(8081);
+*/
+//Nivel 4
