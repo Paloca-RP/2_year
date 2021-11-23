d30acbc9f258a258c4912a1f3ec1ef971b4e74d2
Ricardo Palhoça <73611099+Paloca-RP@users.noreply.github.com>
2021-11-23 22:55:40 +0000

    ads

---
+++ Programacao_Aplicacoes_Moveis/exemplo_multipages/app/src/main/java/com/example/bmi/activity_result.java
@@ -0,0 +1,14 @@
+package com.example.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
