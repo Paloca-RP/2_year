76ddf781ff4f13126ae83999a9c3d9c1ff4ff238
Ricardo Palhoça <73611099+Paloca-RP@users.noreply.github.com>
2021-11-26 19:41:49 +0000

    az

---
+++ Programacao_Visual/Semana07/S07_1_Tutorial_LinQ_Materiais/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TP06_TutorialLinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("XXXXXXXXXXX 1 TESTE DA LISTA");
+            Alunos alunos = new Alunos();
+            Console.WriteLine(alunos.ToString());
+
+            Console.ReadKey();
+
+            Console.WriteLine("XXXXXXXXXXX 2 Todos os nomes de alunos");
+            
+
+        }
+    }
+}
